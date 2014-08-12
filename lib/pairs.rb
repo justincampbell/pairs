713a9ed2d58c0a3ddd1377546c40e8837bc944e9
Justin Campbell <justin@justincampbell.me>
2014-08-12 19:26:37 -0400

    Basic pairing

--- lib/pairs.rb
+++ lib/pairs.rb
@@ -1,5 +1,48 @@
-require "pairs/version"
+class Pairs
+  attr_reader :block
 
-module Pairs
-  # Your code goes here...
+  def initialize(&block)
+    @block = block
+  end
+
+  def solve!
+    clean_room.instance_exec(&block)
+
+    all = clean_room.__items__.values.flatten
+    result = []
+
+    until all.empty?
+      these = all.sample(2)
+      result << these
+
+      these.each do |item|
+        all.delete item
+      end
+    end
+
+    result
+  end
+
+  def solution
+    @solution ||= solve!
+  end
+
+  private
+
+  def clean_room
+    @clean_room ||= CleanRoom.new
+  end
+
+  class CleanRoom
+    def __items__
+      @__items__ ||= {}
+    end
+
+    def method_missing(method_name, *args, &block)
+      return super unless args.count == 1
+
+      __items__[method_name] ||= []
+      __items__[method_name] << args.first
+    end
+  end
 end
--- spec/lib/pairs_spec.rb
+++ spec/lib/pairs_spec.rb
@@ -1,11 +1,53 @@
 require 'spec_helper'
 
 describe Pairs do
-  it "works" do
-    expect(Pairs).to be_a(Module)
+  let(:pairs) { Pairs.new(&block) }
+  let(:block) {
+    -> { }
+  }
+  let(:solution) { pairs.solution }
+
+  context "without a block" do
+    it "returns an empty solution" do
+      expect(solution).to eq([])
+    end
   end
 
-  generative do
-    it { expect(1).to eq(1) }
+  context "and 1 item" do
+    let(:block) { -> { thing "a" } }
+
+    it "returns that item" do
+      expect(solution).to eq([["a"]])
+    end
+  end
+
+  context "and 2 items" do
+    let(:block) { -> { thing "a"; thing "b" } }
+
+    generative do
+      it "returns both items" do
+        expect(solution.flatten).to match_array(%w[a b])
+      end
+    end
+  end
+
+  context "and 3 items" do
+    let(:block) {
+      -> {
+        thing "a"
+        thing "b"
+        thing "c"
+      }
+    }
+
+    generative do
+      it "has all items" do
+        expect(solution.flatten).to match_array(%w[a b c])
+      end
+
+      it "groups them in pairs" do
+        expect(solution.first.size).to eq(2)
+      end
+    end
   end
 end
